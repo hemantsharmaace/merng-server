707e43a1c7ce7f79b2a780da75b772175956867a
hemantsharmaace <hemantsharmaece@gmail.com>
2022-07-26 01:13:19 +0530

    fix issue

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB : "mongodb+srv://user-123:user-123@myreactmern.cgy6n.mongodb.net/socialmediapp?retryWrites=true&w=majority",
+    SECRETKEY : "some sory of secrey key"
+}
